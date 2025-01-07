7dcf6d89995ef34cd744a059cf8d98f0e9525b9a
m.fadhilakbar <muhammadfadhilakbar10@gmail.com>
2025-01-07 09:37:51 +0700

    test: open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,10 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
       </head>
       <body className={mainFont.className}>{children}</body>
     </html>
