7f9924e42c3964d57677e069eed26cde98bb3617
m.fadhilakbar <muhammadfadhilakbar10@gmail.com>
2025-01-18 14:38:59 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,20 @@
 
 import { ParallaxClosed, ParallaxOpened } from "@/components/parallax";
 import { useEffect, useRef, useState } from "react";
+import BranchTreeImage from "@/assets/images/branch-tree-behind-mosque-copy-min.png";
+import CloudImage from "@/assets/images/cloud-behind-mosque-copy-min.png";
 import { Container } from "@/components/ui";
-import audio1 from "../../public/audio1.mp3";
-import { useMotionSource } from "@/hooks";
 import Image from "next/image";
 import MosqueImage from "@/assets/images/mosque-copy-min.png";
 import { MotionWrapper } from "@/components/animate";
+import OrnamentOmbakImage from "@/assets/images/ornament-ombak-copy-min.png";
 import PinusImage from "@/assets/images/pinus-behind-mosque-copy-min.png";
+import audio1 from "../../public/audio1.mp3";
+import { useMotionSource } from "@/hooks";
+// import CloudBg from "@/assets/images/cloud2.png";
+// import FlowerOmbakImage from "@/assets/images/flower-above-ombak-copy-min.png";
+// import GardenImage from "@/assets/images/garden-infront-mosque-copy-min.png";
+// import LeafTreeImage from "@/assets/images/leaf-tree-behind-mosque-copy-min.png";
 
 export default function Home() {
   const { matchMedia } = useMotionSource();
@@ -33,15 +40,37 @@ export default function Home() {
           `}
         >
           <audio ref={audioRef} autoPlay loop>
-            {/* <source src={audio1} type="audio/mp3" /> */}
+            <source src={audio1} type="audio/mp3" />
           </audio>
           {render &&
             (!opened ? (
               <ParallaxClosed ref={audioRef} onOpen={() => setOpened(true)} />
             ) : (
               <>
-                <div className="h-[100vh] w-full bg-[red] relative">
+                <div className="h-[100vh] w-full relative">
                   {/* <div style={{ position: "absolute", top: 0 }}> */}
+                  <MotionWrapper
+                    initial={{ opacity: 1, scale: 1 }}
+                    animate={{ opacity: 0.2, scale: 1.3 }}
+                    transition={{ duration: 1.8 }}
+                    motionProps={{
+                      layout: true,
+                    }}
+                    style={{ position: "absolute" }}
+                  >
+                    <Image priority alt="cloud" src={CloudImage} />
+                  </MotionWrapper>
+                  <MotionWrapper
+                    initial={{ opacity: 1, scale: 1 }}
+                    animate={{ opacity: 0.5, scale: 1.3 }}
+                    transition={{ duration: 2.5 }}
+                    motionProps={{
+                      layout: true,
+                    }}
+                    style={{ position: "absolute" }}
+                  >
+                    <Image priority alt="branch tree" src={BranchTreeImage} />
+                  </MotionWrapper>
                   <MotionWrapper
                     initial={{ opacity: 1, scale: 1 }}
                     animate={{ opacity: 0.5, scale: 1.3 }}
@@ -63,6 +92,17 @@ export default function Home() {
                     style={{ position: "absolute", top: "135px" }}
                   >
                     <Image priority alt="mosque" src={MosqueImage} />
+                  </MotionWrapper>
+                  <MotionWrapper
+                    initial={{ opacity: 1 }}
+                    animate={{ opacity: 0.5 }}
+                    transition={{ duration: 2 }}
+                    motionProps={{
+                      layout: true,
+                    }}
+                    style={{ position: "absolute", bottom: 0 }}
+                  >
+                    <Image priority alt="mosque" src={OrnamentOmbakImage} />
                   </MotionWrapper>
                   {/* </div> */}
                 </div>
